26b852b61e711e38c3ed040537e96c946a42e7ba
swedendrift <swedendrift@gmail.com>
2016-11-22 11:21:42 -0800

    working on search

--- js/default.js
+++ js/default.js
@@ -1,27 +1,49 @@
-//create an array of objects to represent the videos (pictures right now)
-var videos = [{title: "Surfing", videographer: "Mowgly Lee", descriptoin: 'Nulla facilisi. Sed vel erat ut augue sollicitudin elementum vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/surf.jpg"},
-              {title: "Beer Pong", videographer: "Tin Dein", descriptoin: 'Nulla facilisi. Sed vel erat ut augue sollicitudin elementum vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/beerPong.jpg"},
-              {title: "Nascar", videographer: "Robert Denton", descriptoin: 'Nulla facilisi. Sed vel erat ut augue sollicitudin elementum vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/nascar.jpg"},
-              {title: "Volleyball", videographer: "Bruce Chindo", descriptoin: 'Nulla facilisi. Sed vel erat ut augue sollicitudin elementum vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/volleyball.jpg"},
-              {title: "Surf", videographer: "Chris Clark", descriptoin: 'Nulla facilisi. Sed vel erat ut augue sollicitudin elementum vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/surf2.jpg"}];
+//create an array of objects to represent the videos (pictures right atm)
+var videos = [{title: "Surf", videographer: "Mowgly Lee", description: 'Nulla facilisi. Sed vel erat ut augue surf surfing vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/surf.jpg"},
+              {title: "Beer Pong", videographer: "Tin Dein", description: 'Nulla facilisi. Sed vel drunk ut augue beer pong party vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/beerPong.jpg"},
+              {title: "Nascar", videographer: "Robert Denton", description: 'Nulla facilisi. Sed vel car ut augue nascar elementum vel eu crash. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/nascar.jpg"},
+              {title: "Volleyball", videographer: "Bruce Chindo", description: 'Nulla volleyball vel erat ut augue volley ball elementum vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/volleyball.jpg"},
+              {title: "Surf", videographer: "Chris Clark", description: 'Nulla facilisi. Sed vel surf ut augue sollicitudin elementum vel surfing. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/surf2.jpg"}];
 
 
-
+// function to add new video results to the page
 function addNewItem() {
-    var newItem = document.createElement('li');
-    var newImg = document.createElement('img');
-      newImg.setAttribute('src', 'imgs/beerPong.jpg');
-    var newText = document.createTextNode(videos[0].title);
-    newItem.appendChild(newImg);
-    newItem.appendChild(newText);
 
-    var position = document.getElementById('searchList');
-    position.appendChild(newItem);
+  //create a new li, img, h3 and paragraph elements
+  var newItem = document.createElement('li');
+  var newImg = document.createElement('img');
+  newImg.setAttribute('src', 'imgs/beerPong.jpg');
+  var newHeading = document.createElement('h3');
+  var newP = document.createElement('p');
+
+  // create new text nodes
+  var newHeadingText = document.createTextNode(videos[0].title);
+  var newDescription = document.createTextNode(videos[0].description);
+
+  // attach the text nodes and elements to their parent elements
+  newP.appendChild(newDescription);
+  newHeading.appendChild(newHeadingText);
+  newItem.appendChild(newImg);
+  newItem.appendChild(newHeading);
+  newItem.appendChild(newP);
+  var position = document.getElementById('searchlist');
+  position.appendChild(newItem);
 }
 
-var searchListener = document.getElementById('searchButton');
+// function to search videos for words
+function videoSearch() {
+  var query = document.getElementById('searchquery').textContent;
+  var newh4 = document.createElement('h4');
+  var querytext = document.createTextNode(query);
+  newh4.appendChild(querytext);
 
-searchListener.addEventListener('click', addNewItem, false);
+  var position = document.getElementById('queryecho');
+  position.appendChild(newh4);
+  addNewItem()
+}
+
+var searchListener = document.getElementById('searchbutton');
+searchListener.addEventListener('click', videoSearch, false);
 
 // <li>
 //   <img src="imgs/surf.jpg" />
