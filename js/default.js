56653040426750c7ac9d54034267593710ae9a55
swedendrift <swedendrift@gmail.com>
2016-11-23 14:59:49 -0800

    Youtube API query returning data that has been placed into an object for each video search item returned

--- js/default.js
+++ js/default.js
@@ -1,57 +1,58 @@
-var videos = [{title: "surf", videographer: "Mowgly Lee", description: 'Nulla facilisi. Sed vel erat ut augue surf surfing vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/surf.jpg"},
-{title: "beer pong", videographer: "Tin Dein", description: 'Nulla facilisi. Sed vel drunk ut augue beer pong party vel eu nibh. Pellentesque nascar dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/beerPong.jpg"},
-{title: "nascar", videographer: "Robert Denton", description: 'Nulla facilisi. Sed vel car ut augue nascar elementum vel eu crash. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/nascar.jpg"},
-{title: "volleyball", videographer: "Bruce Chindo", description: 'Nulla volleyball vel erat ut augue volley ball elementum vel eu nibh. Pellentesque pellentesque dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/volleyball.jpg"},
-{title: "surf", videographer: "Chris Clark", description: 'Nulla facilisi. Sed vel surf ut augue sollicitudin elementum vel surfing. Pellentesque drunk dolor felis, at iaculis sem blandit non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/surf2.jpg"},
-{title: "nascar", videographer: "Robert Denton", description: 'Nulla facilisi. Sed vel car ut augue nascar elementum vel eu crash. Pellentesque pellentesque dolor felis, at iaculis sem surf non. Aenean ut ipsum id ex laoreet finibus a ut.', url: "imgs/nascar.jpg"},];
 
-function addSearchResult(videoObject) {
-  var newItem = document.createElement('li');
-  var newImg = document.createElement('img');
-  newImg.setAttribute('src', videoObject.url);
-  var newHeading = document.createElement('h3');
-  var newP = document.createElement('p');
-  var newHeadingText = document.createTextNode(videoObject.title);
-  var newDescription = document.createTextNode(videoObject.description);
-  newP.appendChild(newDescription);
-  newHeading.appendChild(newHeadingText);
-  newItem.appendChild(newImg);
-  newItem.appendChild(newHeading);
-  newItem.appendChild(newP);
-  var position = document.getElementById('searchlist');
-  position.appendChild(newItem);
-}
 
-function videoSearch() {
+
+function searchRequest() {
   clearDOM();
-  // show the search query under the search box
-  var query = document.getElementById('searchquery');
-  var newh2 = document.createElement('h2');
-  var querytext = document.createTextNode(query.value);
-  newh2.appendChild(querytext);
-  var position = document.getElementById('queryecho');
-  position.appendChild(newh2);
-  // loop through the array placing content as its found
-  for (var i = 0; i < videos.length; i++) {
-    var searchText = query.value;
-    var itemText = videos[i].title + videos[i].description;
-    var match = itemText.toLowerCase().indexOf(searchText.toLowerCase()) > -1
-    if (searchText !== "" && match) {
-      addSearchResult(videos[i]);
-    }
-  }
+  var queryElement = document.getElementById('searchquery')
+  var queryString = queryElement.value;
+  var url = encodeURI('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + queryString + '&key=AIzaSyCTzbJhZboKUo5J4DX7iMNOTBUXEEIo6pU')
+  $.get(url, function(data) {
+    console.log(data);
+    addSearchResult(data);
+  });
 }
 
 function clearDOM() {
-  var searchElements = document.getElementById('searchlist');
+  var searchElements = document.getElementById('search-list');
   while (searchElements.firstChild) {
     searchElements.removeChild(searchElements.firstChild);
   }
-  var echoElements = document.getElementById('queryecho');
+  var echoElements = document.getElementById('alert-echo');
   while (echoElements.firstChild) {
     echoElements.removeChild(echoElements.firstChild);
   }
 }
 
+function addSearchResult(videoObjects) {
+
+  for (var i = videoObjects.length; i > 0; i--) {
+    // set key obj values to local variables for DOM stuffage
+    var videoMatch = {
+     videoId: videoObjects[i].items.id.videoid,
+     publishedAt: videoObjects[i].items.snippet.publishedAt,
+     title: videoObjects[i].items.snippet.title,
+     description: videoObjects[i].items.snippet.description,
+     thumbnail: videoObjects[i].items.snippet.thumbnails.medium.url
+    }
+
+    console.log(videoMatch);
+
+    // var newItem = document.createElement('li');
+    // var newImg = document.createElement('img');
+    // newImg.setAttribute('src', videoMatch.thumbnail);
+    // var newHeading = document.createElement('h3');
+    // var newP = document.createElement('p');
+    // var newHeadingText = document.createTextNode(videoMatch.title);
+    // var newDescription = document.createTextNode(videoMatch.description);
+    // newP.appendChild(newDescription);
+    // newHeading.appendChild(newHeadingText);
+    // newItem.appendChild(newImg);
+    // newItem.appendChild(newHeading);
+    // newItem.appendChild(newP);
+    // var position = document.getElementById('search-list');
+    // position.appendChild(newItem);
+  }
+}
+
 var searchListener = document.getElementById('searchbutton');
-searchListener.addEventListener('click', videoSearch, false);
+searchListener.addEventListener('click', searchRequest, false);
