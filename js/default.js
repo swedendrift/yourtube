58155c9412c5e9de519c2f54646fd7c9d33806a2
swedendrift <swedendrift@gmail.com>
2016-11-23 16:57:25 -0800

    end of the day commit.

--- js/default.js
+++ js/default.js
@@ -1,41 +1,40 @@
-
-
-
 function searchRequest() {
-  clearDOM();
+  // clearDOM();
   var queryElement = document.getElementById('searchquery')
   var queryString = queryElement.value;
   var url = encodeURI('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + queryString + '&key=AIzaSyCTzbJhZboKUo5J4DX7iMNOTBUXEEIo6pU')
   $.get(url, function(data) {
-    console.log(data);
-    addSearchResult(data);
+    addSearchResult(data.items);
   });
 }
 
-function clearDOM() {
-  var searchElements = document.getElementById('search-list');
-  while (searchElements.firstChild) {
-    searchElements.removeChild(searchElements.firstChild);
-  }
-  var echoElements = document.getElementById('alert-echo');
-  while (echoElements.firstChild) {
-    echoElements.removeChild(echoElements.firstChild);
-  }
-}
+// function clearDOM() {
+//   var searchElements = document.getElementById('search-list');
+//   while (searchElements.firstChild) {
+//     searchElements.removeChild(searchElements.firstChild);
+//   }
+//   var echoElements = document.getElementById('alert-echo');
+//   while (echoElements.firstChild) {
+//     echoElements.removeChild(echoElements.firstChild);
+//   }
+// }
 
-function addSearchResult(videoObjects) {
+function addSearchResult(results) {
+  console.log(results);
+  var i = 0;
+  // for (i = results.length: i > 0; i--) {
+    var videoId = results[i].id.videoId
+    var publishedAt = results[i].snippet.publishedAt
+    var title = results[i].snippet.title
+    var description = results[i].snippet.description
+    var thumbnail = results[i].snippet.thumbnails.medium.url
+  // }
+  //would be better to loop through and push into an array for later display
+  console.log(videoId);
+  console.log(thumbnail);
 
-  for (var i = videoObjects.length; i > 0; i--) {
-    // set key obj values to local variables for DOM stuffage
-    var videoMatch = {
-     videoId: videoObjects[i].items.id.videoid,
-     publishedAt: videoObjects[i].items.snippet.publishedAt,
-     title: videoObjects[i].items.snippet.title,
-     description: videoObjects[i].items.snippet.description,
-     thumbnail: videoObjects[i].items.snippet.thumbnails.medium.url
-    }
 
-    console.log(videoMatch);
+
 
     // var newItem = document.createElement('li');
     // var newImg = document.createElement('img');
@@ -51,7 +50,7 @@ function addSearchResult(videoObjects) {
     // newItem.appendChild(newP);
     // var position = document.getElementById('search-list');
     // position.appendChild(newItem);
-  }
+  // }
 }
 
 var searchListener = document.getElementById('searchbutton');
