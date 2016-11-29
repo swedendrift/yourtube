ddbdf0f7f97325829f277100b2162efdf7bb0908
swedendrift <swedendrift@gmail.com>
2016-11-29 11:25:41 -0800

    issue-3-comments passing and returning data from comments array

--- js/default.js
+++ js/default.js
@@ -1,18 +1,3 @@
-var resultsArray = [];
-
-function searchRequest() {
-  clearDOM();
-  var queryElement = document.getElementById('searchquery')
-  var queryString = queryElement.value;
-  var url = encodeURI('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + queryString + '&key=')
-  if (queryString) {
-    $.get(url, function(data) {
-      console.log(data);
-      addSearchResults(data.items);
-    });
-  }
-}
-
 function clearDOM() {
   var ytContainer = document.getElementById('yt-container');
   while (ytContainer.firstChild) {
@@ -28,12 +13,17 @@ function clearDOM() {
   }
 }
 
-function ResultConstructor(videoId, publishedAt, title, description, thumbUrl) {
-  this.videoId = videoId;
-  this.publishedAt = publishedAt;
-  this.title = title;
-  this.description = description;
-  this.thumbUrl = thumbUrl;
+function searchRequest() {
+  clearDOM();
+  var queryElement = document.getElementById('searchquery')
+  var queryString = queryElement.value;
+  var url = encodeURI('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + queryString + '&key=AIzaSyCTzbJhZboKUo5J4DX7iMNOTBUXEEIo6pU')
+  if (queryString) {
+    $.get(url, function(data) {
+      console.log(data);
+      addSearchResults(data.items);
+    });
+  }
 }
 
 function addSearchResults(results) {
@@ -44,11 +34,19 @@ function addSearchResults(results) {
       var title = results[i].snippet.title
       var description = results[i].snippet.description
       var thumbnail = results[i].snippet.thumbnails.medium.url
-      var currentResult = new ResultConstructor(videoId, date, title, description, thumbnail);
-      resultsArray.push(currentResult);
+      var currentResult = new Result(videoId, date, title, description, thumbnail);
+      resultsCollection.push(currentResult);
       resultsBuilder(currentResult);
     }
   }
+}
+
+function Result(videoId, publishedAt, title, description, thumbUrl) {
+  this.videoId = videoId;
+  this.publishedAt = publishedAt;
+  this.title = title;
+  this.description = description;
+  this.thumbUrl = thumbUrl;
 }
 
 function resultsBuilder(currentResult) {
@@ -82,6 +80,7 @@ function playVideo(videoId) {
   clearDOM()
   var videoUrl = 'https://www.youtube.com/embed/' + videoId + '?enablejsapi=1&fs=1&origin=http://localhost"frameborder="0"'
   playerBuilder(videoUrl);
+  commentBuilder(videoId);
 }
 
 function playerBuilder(videoUrl) {
@@ -94,6 +93,105 @@ function playerBuilder(videoUrl) {
   resultsContainer.appendChild(newIFrame);
 }
 
+function commentBuilder(videoId) {
+  var commentMatches = findMatches(videoId);
+  if (commentMatches.length > 0) {
+    showExistingComments(videoId, commentMatches);
+    console.log(commentMatches);
+  }
+}
+
+
+function findMatches(videoId) {
+  var matches = [];
+  for (var i = 0; i < commentsCollection.length; i++) {
+    if (commentsCollection[i].videoId === videoId) {
+      matches.push(commentsCollection[i]);
+    }
+  }
+  return matches;
+}
+
+function showExistingComments(videoId, commentMatches) {
+  var commentsContainer = document.getElementById('comments-container');
+  var commentInputDiv = document.createElement('div');
+  commentInputDiv.setAttribute('class', videoId);
+  var inputBox = document.createElement('input');
+  inputBox.setAttribute('id', 'new-comment');
+  inputBox.setAttribute('placeholder', "Add a comment...");
+  inputBox.setAttribute('type', 'text');
+  inputBox.setAttribute('class', 'input-bar');
+
+  var inputBtn = document.createElement('button');
+  inputBtn.setAttribute('id', 'commentBtn')
+  inputBtn.setAttribute('value', 'comment')
+  inputBtn.setAttribute('type', 'text');
+
+  var hr = document.createElement('hr');
+
+  commentInputDiv.appendChild(inputBox)
+  commentInputDiv.appendChild(inputBtn);
+  commentInputDiv.appendChild(hr);
+  commentsContainer.insertBefore(commentInputDiv, commentsContainer.childNodes[0]);
+
+  console.log(commentMatches[0].videoId);
+
+
+  if (commentMatches.length > 0) {
+    for (var i = 0; i < commentMatches.length; i++) {
+      var commentThreads = document.getElementById('comment-threads');
+      var newLi = document.createElement('li');
+      var newHeading = document.createElement('h5');
+      var newP = document.createElement('p');
+      var commentId = document.createTextNode(commentMatches[i].videoId);
+      var commentText = document.createTextNode(commentMatches[i].commentString)
+      commentThreads.appendChild(newLi);
+      newHeading.appendChild(commentId);
+      newP.appendChild(commentText);
+      newLi.appendChild(newHeading);
+      newLi.appendChild(newP);
+    }
+  }
+
+
+  var commentListener = document.getElementById('commentBtn');
+  commentListener.addEventListener('click', addComment(commentMatches.videoId), false);
+}
+
+function addComment(videoId) {
+
+  // var comment = document.getElementById('new-comment');
+  // var commentString = comment.value;
+  // var datePosted = getTimeStamp()
+  // var currentComment = new Comment(videoId, datePosted, commentString);
+  // // add email and a user array
+  // commentsCollection.push(currentComment)
+  // showComments()
+  // console.log(currentComment);
+
+}
+//
+// function Comment(videoId, datePosted, commentString) {
+//   this.videoId = videoId;
+//   this.datePosted = datePosted;
+//   this.commentString = commentString;
+// }
+
+
+
+// function getTimeStamp() {
+//   var now = new Date();
+//   return ((now.getMonth() + 1) + '/' + (now.getDate()) + '/' + now.getFullYear() + " " + now.getHours() + ':' + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now.getSeconds()) : (now.getSeconds())));
+// }
+
+
+var resultsCollection = [];
+var commentsCollection = [{videoId: "tntOCGkgt98", datePosted: "2013-12-31T05:21:57.000Z", commentString: "this is the coolest site ever"},
+                          {videoId: "htOroIbxiFY", datePosted: "2016-07-08T13:41:40.000Z", commentString: "this is the sickest site ever"},
+                          {videoId: "tntOCGkgt98", datePosted: '2013-12-31T05:21:57.000Z', commentString: "this is the baddest site ever"},
+                          {videoId: "htOroIbxiFY", datePosted: "2016-07-08T13:41:40.000Z", commentString: "this is the raddest site ever"},
+                          {videoId: "G8KpPw303PY", datePosted: "2016-07-08T13:41:40.000Z", commentString: "this is the gnarliest site ever"}];
+// var users = [];
 
 var searchListener = document.getElementById('searchbutton');
 searchListener.addEventListener('click', searchRequest, false);
